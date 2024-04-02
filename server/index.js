8ac7f50eb8a4c7d8a3dc09ca063eec4b3649fca0
Ayo <heyayomideadebisi@gmail.com>
2024-04-02 14:14:33 +0100

    build: add cookieparser and express session

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 import express from "express";
 import prisma from "./lib/db.js";
 import authRouter from "./routes/authRouter.js";
+const session = require("express-session");
 const app = express()
 
 app.use(express.json())
 app.use("/", authRouter)
+
+app.use(
+  session({
+      secret: "your-secret-key",
+      resave: false,
+      saveUninitialized: false,
+  })
+);
 
 app.get("/", (req, res) => {
   res.json({});
