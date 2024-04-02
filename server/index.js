a1d130b9f0b7a12752844bf5144f7f7aa8439d03
Ayo <heyayomideadebisi@gmail.com>
2024-04-02 02:15:39 +0100

    chore:test query done successfully

---
+++ server/index.js
@@ -0,0 +1,27 @@
+// import { PrismaClient } from '@prisma/client'
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  const user = await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alasicdfkne@prisma.io'
+    },
+  })
+  console.log(user);
+  
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+    console.log("done");
+    
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
