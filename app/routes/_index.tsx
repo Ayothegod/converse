bb5a6b682eec526b0f1b1425ad8bc29a6284ae2b
Ayo <heyayomideadebisi@gmail.com>
2024-04-04 11:57:24 +0100

    task: install prisma

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,35 +7,13 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+// "postgresql://postgres:postgrespass@localhost:5432/babblechat?schema=public"
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="">
+      <h1 className="text-4xl text-purple-600">Welcome to Remix</h1>
     </div>
   );
 }
+// prisma init
