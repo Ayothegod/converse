385147306f80d9e850b98b8911ac2054e81f878d
Ayo <heyayomideadebisi@gmail.com>
2024-04-04 16:37:44 +0100

    build: add our validation packages

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -3,11 +3,33 @@ import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import { Form } from "@remix-run/react";
 import { authenticator } from "~/services/auth.server";
 
+// login or signup
+export async function action({ request }: ActionFunctionArgs) {
+    let formData = await request.formData();
+  return await authenticator.authenticate("user-pass", request, {
+
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+    context: { formData }, 
+  });
+}
+
+// check if user session already exists
+export async function loader({ request }: LoaderFunctionArgs) {
+  return await authenticator.isAuthenticated(request, {
+    successRedirect: "/dashboard",
+  });
+}
+
 export default function Screen() {
   return (
     <Form method="post">
-        
-      <input type="email" name="email" className="border border-blacK" required />
+      <input
+        type="email"
+        name="email"
+        className="border border-blacK"
+        required
+      />
       <input
         className="border border-blacK"
         type="password"
@@ -20,16 +42,3 @@ export default function Screen() {
   );
 }
 
-export async function action({ request }: ActionFunctionArgs) {
-  return await authenticator.authenticate("user-pass", request, {
-    successRedirect: "/dashboard",
-    failureRedirect: "/login",
-  });
-}
-
-export async function loader({ request }: LoaderFunctionArgs) {
-  // If the user is already authenticated redirect to /dashboard directly
-  return await authenticator.isAuthenticated(request, {
-    successRedirect: "/dashboard",
-  });
-}
