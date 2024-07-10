e139baf53839b007562ddd22918e619a7fdb8ca1
Ayothegod <heyayomideadebisi@gmail.com>
2024-07-10 21:52:54 +0100

    remove remix code

--- app/routes/action.set-theme.ts
+++
@@ -1,4 +0,0 @@
-import { createThemeAction } from "remix-themes"
-import { themeSessionResolver } from "~/services/themeSession.server"
-
-export const action = createThemeAction(themeSessionResolver)
--- prisma/migrations/20240410055613_add_user_info/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "bio" TEXT,
-ADD COLUMN     "defaultImageUrl" TEXT,
-ADD COLUMN     "fullname" TEXT,
-ADD COLUMN     "punchline" TEXT;
